6fb241f9b09e090aa77c6609e01d2df18d345dfc
Christopher Brown <io@henrian.com>
2015-06-03 15:41:07 -0500

    Replace tap testing framework with mocha

--- test/server.js
+++ test/server.js
@@ -1,38 +1,46 @@
-'use strict'; /*jslint es5: true, node: true, indent: 2 */
-var tap = require('tap');
+/*globals describe, it, before, after */
+var http = require('http');
+var assert = require('assert');
 var request = require('request');
-
 var Router = require('..');
-tap.test('basic server', function(t) {
-  t.plan(2);
 
-  // set up the routes
-  var R = new Router(function(req, res) {
-    res.end('404 :: URL not found.');
-  });
-  R.get(/^\/page\/(\w+)/, function(req, res, m) {
-    res.end('Fetching page "' + m[1] + '"');
-  });
-
-  // set up the server
-  var hostname = '127.0.0.1';
-  var server = require('http').createServer(function(req, res) {
-    R.route(req, res);
-  }).listen(0, hostname, function() { // port = 0 finds random free port
-    // server running, ready to run tests
-    var port = server.address().port;
-    var addrport = 'http://' + hostname + ':' + port;
-
-    request.get(addrport + '/page/contact', function(err, res, body) {
-      t.equal(body, 'Fetching page "contact"', 'Need to fetch page by name.');
+describe('basic server', function() {
+  var server;
+  before(function(callback) {
+    // set up the routes
+    var R = new Router();
+    R.get(/^\/page\/(\w+)/, function(req, res, m) {
+      res.end('Fetching page "' + m[1] + '"');
     });
-
-    request.get(addrport + '/reference', function(err, res, body) {
-      t.equal(body, '404 :: URL not found.', 'Need to return 404 page.');
+    // set up the server
+    var hostname = '127.0.0.1';
+    server = http.createServer(function(req, res) {
+      R.route(req, res);
+    }).listen(0, hostname, function() { // port = 0 finds random free port
+      // server running, ready to run tests
+      var port = server.address().port;
+      request = request.defaults({baseUrl: 'http://' + hostname + ':' + port + '/'});
+      callback();
     });
   });
 
-  t.tearDown(function() {
+  after(function() {
     server.close();
+  });
+
+  it('should fetch page by name', function(callback) {
+    request.get({uri: '/page/contact'}, function(err, res, body) {
+      if (err) return callback(err);
+      assert.equal(body, 'Fetching page "contact"');
+      callback();
+    });
+  });
+
+  it('should return default "not found" message', function(callback) {
+    request.get({uri: '/reference'}, function(err, res, body) {
+      if (err) return callback(err);
+      assert.equal(body, 'Not Found\n');
+      callback();
+    });
   });
 });
